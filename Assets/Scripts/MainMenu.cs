3a21d8e05ef0a08f2526d203008a0245fa94cd1a
Samoustique <samuel.philipps@gmail.com>
2016-03-20 23:51:13 +0100

    [Alderaans] SkyBox and MapHUD

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -4,9 +4,32 @@ using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class MainMenu : MonoBehaviour {
-	
+
+	private Animator animator;
+	private GameObject mainCamera;
+	private Transform menuPositionTransform;
+	private GameObject refCam;
+
+	public void Start()
+	{
+		mainCamera = GameObject.Find ("Main Camera");
+		refCam = GameObject.Find ("RefCam");
+		animator = ((Animator)mainCamera.GetComponent<Animator>());
+		menuPositionTransform = mainCamera.transform;
+	}
+
 	public void LoadLevel()
 	{
 		SceneManager.LoadScene ("towerDefense");
+	}
+
+	public void DisplayHistory()
+	{		
+		animator.Play("MenuCamHistoryAnim", -1, 0F);
+	}
+
+	public void DisplayMainMenu()
+	{		
+		animator.Play("MenuCamMainMenuAnim", -1, 0F);
 	}
 }
