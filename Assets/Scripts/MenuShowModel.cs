9c324d22cb4511f1cd4d6510965c0f3dc3ad3326
Samoustique <samuel.philipps@gmail.com>
2016-03-22 00:10:15 +0100

    [Alderaans] Beginning of Scenario and new skybox

---
+++ Assets/Scripts/MenuShowModel.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuShowModel : MonoBehaviour {
+	void Update ()
+	{
+		Vector3 vec = new Vector3(UnityEngine.Random.Range(0, 2),UnityEngine.Random.Range(0, 2),UnityEngine.Random.Range(0, 2));
+		transform.Rotate(vec * Time.deltaTime * 5);
+	}
+}
